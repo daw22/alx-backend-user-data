7f5f2561663f5a8c4384aa3cf3175b02e6dda4dc
dawit <dawitxxd@gmail.com>
2024-08-29 21:23:30 +0300

    Check valid password

--- 0x00-personal_data/encrypt_password.py
+++ 0x00-personal_data/encrypt_password.py
@@ -10,7 +10,15 @@ def hash_password(password: str) -> bytes:
     Returns a salted, hashed password which is
     a byte string
     """
-    enc_password = passwd.encode()
+    enc_password = password.encode()
     hashed_passwd = bcrypt.hashpw(enc_password, bcrypt.gensalt())
 
     return hashed_passwd
+
+
+def is_valid(hashed_password: bytes, password: str) -> bool:
+    """
+    Validate password
+    """
+    encoded_pwd = password.encode()
+    return bcrypt.checkpw(encoded_pwd, hashed_password)
