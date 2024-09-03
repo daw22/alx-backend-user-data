98bf28b06b3bb6e590d544d26607132a062d63c8
dawit <dawitxxd@gmail.com>
2024-09-03 09:55:25 +0300

    Request validation

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -27,7 +27,11 @@ class Auth:
     def authorization_header(self, request=None) -> str:
         """ Not implemented yet
         """
-        return None
+        if request is None:
+            return None
+        if 'Authorization' not in request.headers:
+            return None
+        return request.headers.get('Authorization')
 
     def current_user(self, request=None) -> TypeVar('User'):
         """ Not implemented yet
