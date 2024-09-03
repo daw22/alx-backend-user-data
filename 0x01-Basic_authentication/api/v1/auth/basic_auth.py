5882f582fd7384228e729d6d84975767f5831db3
dawit <dawitxxd@gmail.com>
2024-09-03 13:29:45 +0300

    Bacic- User credentials

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -40,3 +40,18 @@ class BasicAuth(Auth):
             return decoded.decode("utf-8")
         except Exception:
             return None
+
+    def extract_user_credentials(self, decoded_base64_authorization_header: str
+                                 ) -> (str, str):
+        """
+        Returns the user email and password from the
+        baseb4 decoded value
+        """
+        if decoded_base64_authorization_header is None:
+            return (None, None)
+        if not isinstance(decoded_base64_authorization_header, str):
+            return (None, None)
+        if ":" not in decoded_base64_authorization_header:
+            return (None, None)
+        credentials = decoded_base64_authorization_header.split(":")
+        return (credentials[0], credentials[1])
