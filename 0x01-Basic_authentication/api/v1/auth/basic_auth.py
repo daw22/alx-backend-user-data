8d2d48299eaaaee4863aedc7206ea86c46f8acca
dawit <dawitxxd@gmail.com>
2024-09-03 10:04:53 +0300

    Basic Auth

---
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""
+Basic Auth class
+"""
+from api.v1.auth.auth import Auth
+
+
+class BasicAuth(Auth):
+    """
+    Basic Auth class, inherits from Auth
+    """
+    pass
