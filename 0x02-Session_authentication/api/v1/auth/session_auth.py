362691289ee185337099ca1b579fa339950df844
dawit <dawitxxd@gmail.com>
2024-09-05 12:37:01 +0300

    Empty session

---
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""
+Session based authenitaction
+"""
+from api.v1.auth.auth import Auth
+
+
+class SessionAuth(Auth):
+    """
+    class implementing session based authentication
+    """
