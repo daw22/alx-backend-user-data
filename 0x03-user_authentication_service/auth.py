0e0523e9021ce610061b554b84bef65498f5e25e
dawit <dawitxxd@gmail.com>
2024-09-11 19:52:55 +0300

    wthgit add auth.py

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -88,10 +88,9 @@ class Auth:
 
         try:
             user = self._db.find_user_by(session_id=session_id)
+            return user
         except NoResultFound:
             return None
-
-        return user
 
     def destroy_session(self, user_id: int) -> None:
         """
