7d35aa203913cd35439ddd4e59f45a08f2852a62
dawit <dawitxxd@gmail.com>
2024-09-11 12:24:32 +0300

    Register user route

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -3,9 +3,10 @@
 flask app
 """
 import flask
-
+from auth import Auth
 
 app = flask.Flask(__name__)
+AUTH = Auth()
 
 
 @app.route('/', methods=["GET"], strict_slashes=False)
@@ -14,6 +15,25 @@ def home():
     basic route
     """
     return flask.jsonify({"message": "Bienvenue"})
+
+
+@app.route('/users', methods=["POST"], strict_slashes=False)
+def register_user():
+    """
+    router to register a user
+    """
+    try:
+        email = flask.request.form['email']
+        password = flask.request.form['password']
+    except KeyError:
+        abort(400)
+
+    try:
+        new_user = AUTH.register_user(email, password)
+    except ValueError:
+        return flask.jsonify({"message": "email already registerd"})
+
+    return flask.jsonify({"email": email, "message": "user created"})
 
 
 if __name__ == "__main__":
